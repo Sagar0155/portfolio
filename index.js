099fe08fcb9fa472f2b05642cdba1cb0fceedd5e
Sagar0155 <158021772+Sagar0155@users.noreply.github.com>
2024-03-19 01:01:03 +0530

    add index.js

---
+++ index.js
@@ -0,0 +1,10 @@
+let a=document.getElementById("home")
+let b=document.getElementById("about")
+let c=document.getElementById("portfi")
+let d=document.getElementById("serv")
+let e=document.getElementById("expi")
+let x=document.getElementById("hom")
+
+a.addEventListener("click",()=>{
+    
+})
